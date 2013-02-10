c7a5ea2f2dece70026a3d2294a830b49ac374c28
Luke Mundy <lmundy@gmail.com>
2013-02-10 16:06:33 +0800

    Finished adding wrapper functions for each type Elophant request.
    Also added two more exceptions for errors thrown from summoner_team_info.

--- pylol/api.py
+++ pylol/api.py
@@ -74,7 +74,12 @@ class Api(object):
         return self.request('champions')
 
     def get_summoner(self, name, region = None):
-        '''Returns name, icon, ID and level of the specified summoner'''
+        '''Returns name, icon, ID and level of the specified summoner
+
+        Arguments:
+        name -- Display name of the summoner to look for.
+        region -- Which region to search in.
+        '''
 
         path = 'summoner/%s' % urllib.quote(name)
 
@@ -93,8 +98,182 @@ class Api(object):
         return ret
 
     def get_mastery_pages(self, sid, region = None):
-        '''Returns all of a Summoner's mastery pages'''
+        '''Returns all of a Summoner's mastery pages
+
+        Arguments:
+        sid -- ID of the summoner to look up (returned from get_summoner()).
+        region -- Which region to search in.
+        '''
 
         path = 'mastery_pages/%d' % sid
+
+        return self.request(path, region)
+
+    def get_rune_pages(self, sid, region = None):
+        '''Returns all of a Summoner's rune pages
+
+        Arguments:
+        sid -- ID of the summoner to look up (returned from get_summoner()).
+        region -- Which region to search in.
+        '''
+
+        path = 'rune_pages/%d' % sid
+
+        return self.request(path, region)
+
+    def get_recent_games(self, aid, region = None):
+        '''Returns all information and stats from a Summoner's last 10 games
+
+        Arguments:
+        aid -- Account ID (not summoner ID) of the summoner to look up.
+        region -- Which region to search in.
+        '''
+
+        path = 'recent_games/%d' % aid
+
+        ret = self.request(path, region)
+
+        # Convert createDate into a proper unix timestamp
+        for k, g in enumerate(ret['gameStatistics']):
+            ts = int(re.sub(r'[^0-9]+', '', g['createDate']))
+            ret['gameStatistics'][k]['createDate'] = ts / 1000.0
+
+        return ret
+
+    def get_summoner_names(self, sids, region = None):
+        '''Returns the names of each summoner
+
+        Arguments:
+        sids -- A list of summoner IDs to look up.
+        region -- Which region to search in.
+        '''
+
+        path = 'summoner_names/%d' % ','.join(sids)
+
+        return self.request(path, region)
+
+    def get_leagues(self, sid, region = None):
+        '''Returns information on each league the summoner is currently in
+
+        Arguments:
+        sid -- ID of the summoner to look up.
+        region -- Which region to search in.
+        '''
+
+        path = 'leagues/%d' % ','.join(sids)
+
+        return self.request(path, region)
+
+    def get_ranked_stats(self, aid, season = None, region = None):
+        '''Returns all information and stats from a Summoner's last 10 games
+
+        Arguments:
+        aid -- Account ID (not summoner ID) of the summoner to look up.
+        season -- Which season to get stats for ('one', 'two' or 'current')
+        region -- Which region to search in.
+        '''
+
+        if season not None:
+            path = 'ranked_stats/%d/%s' % (aid, season)
+        else:
+            path = 'ranked_stats/%d' % aid
+
+        ret = self.request(path, region)
+
+    def get_summoner_team_info(self, sid, region = None):
+        '''Returns information on each team the summoner is currently in
+
+        Arguments:
+        sid -- ID of the summoner to look up.
+        region -- Which region to search in.
+        '''
+
+        path = 'summoner_team_info/%d' % sid
+
+        try:
+            ret = self.request(path, region)
+        except APIFailure as e:
+            if re.search(r'^No teams found for summoner [0-9]+\.$', e.message):
+                raise APISummonerNotInTeams(sid, region)
+            else if re.search(r'^No summoner found with summonerId [0-9]+\.$',
+                              e.message):
+                raise APISummonerIDNotFound(sid, region)
+            else:
+                raise
+
+        return ret
+
+    def get_in_progress_game_info(self, name, region = None):
+        '''Returns player info, picks, bans and observer info for the game
+        the specified summoner is currently in (if any).
+
+        Arguments:
+        name -- Name of the summoner to look up.
+        region -- Which region to search in.
+        '''
+
+        path = 'in_progress_game_info/%s' % name
+
+        try:
+            ret = self.request(path, region)
+        except APIFailure as e:
+            if re.search(r'^Summoner .+ was not found in the system!$',
+                         e.message):
+                raise APISummonerNotFound(name, region)
+            else if re.search(r'^No Game for player .+ was found in the\
+            system!$', e.message):
+                raise APISummonerNotFound(name, region)
+            else:
+                raise
+
+        return ret
+
+    def get_team(self, tid, region = None):
+        '''Returns information on the requested team
+
+        Arguments:
+        tid -- teamId of the team to look up.
+        region -- Which region to search in.
+        '''
+
+        path = 'team/%s' % tid
+
+        return self.request(path, region)
+
+    def get_find_team(self, name, region = None):
+        '''Returns information on the requested team
+
+        Arguments:
+        name -- Name or tag of the team to search for.
+        region -- Which region to search in.
+        '''
+
+        path = 'find_team/%s' % urlib.quote(name)
+
+        return self.request(path, region)
+
+
+    def get_team_end_of_game_stats(self, tid, gid, region = None):
+        '''Returns detailed statistics about the requested ranked match
+
+        Arguments:
+        tid -- Team ID of the team to return stats for.
+        gid -- ID of the game to return stats for.
+        region -- Which region to search in.
+        '''
+
+        path = 'team/%s/end_of_game_stats/%d' % (tid, gid)
+
+        return self.request(path, region)
+
+    def get_team_end_of_game_stats(self, tid, region = None):
+        '''Returns stats of each player in the specified team
+
+        Arguments:
+        tid -- Team ID of the team to return stats for.
+        region -- Which region to search in.
+        '''
+
+        path = 'team/%s/ranked_stats' % tid
 
         return self.request(path, region)
